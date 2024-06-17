3c30f83dc3aa729d63d605757b1c0807b107ec82
ZacharyGo <ZSGoEmpire@gmail.com>
2024-06-18 01:47:06 +0800

    1.10. Link HTML pages from the home page
    Update href to TH:href in all characters in index
    replace all headers and footers of all character to the one in fragments.html

--- src/main/java/com/example/collectibles/controllers/HomeController.java
+++ src/main/java/com/example/collectibles/controllers/HomeController.java
@@ -3,6 +3,7 @@ package com.example.collectibles.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -10,6 +11,11 @@ public class HomeController {
 	@GetMapping("/home")
 	public String displayHome(Model model) {
 		return "index";
+		
+	}
+	@GetMapping("/getCharacter/{charname}")
+	public String getCharacter(@PathVariable("charname") String charName) {
+		return "/characters/"+charName;
 		
 	}
 }
