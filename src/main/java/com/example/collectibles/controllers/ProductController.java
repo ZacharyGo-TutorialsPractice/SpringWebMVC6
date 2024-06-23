d01cbb064524348ff1affbe1270e368f7fd828c7
ZacharyGo <ZSGoEmpire@gmail.com>
2024-06-24 01:30:26 +0800

    2.02. Create a Controller and Java bean for the search function
    Add search-results [given]
    Add Controller ProductController
    Add bean Product (Entity)

---
+++ src/main/java/com/example/collectibles/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package com.example.collectibles.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ProductController {
+
+	@PostMapping("/search")
+	public String search() {
+		return "search-results";
+	}
+}
